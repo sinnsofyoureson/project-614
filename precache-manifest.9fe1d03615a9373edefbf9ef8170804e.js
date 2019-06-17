7cc7da7154d04bc22093d0ca7e49e212e01de3c3
RinatM <sinnsofvalkyrie@gmail.com>
2019-06-17 11:53:39 +0400

    Updates

---
+++ precache-manifest.9fe1d03615a9373edefbf9ef8170804e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4d83cf5fc42f775f57e139c4a32903b",
+    "url": "/project-614/index.html"
+  },
+  {
+    "revision": "5586b0a7b5bd3becd955",
+    "url": "/project-614/static/css/main.49135f5f.chunk.css"
+  },
+  {
+    "revision": "b2851224da2dce121660",
+    "url": "/project-614/static/js/2.35d5a5b6.chunk.js"
+  },
+  {
+    "revision": "5586b0a7b5bd3becd955",
+    "url": "/project-614/static/js/main.036ccf82.chunk.js"
+  },
+  {
+    "revision": "00cbf5653764e500f580",
+    "url": "/project-614/static/js/runtime~main.4428c470.js"
+  },
+  {
+    "revision": "b6065e968c887b68796e4b78dc8a0609",
+    "url": "/project-614/static/media/background.b6065e96.jpg"
+  },
+  {
+    "revision": "15aa058167caac2c8bf4275336646824",
+    "url": "/project-614/static/media/w-mercury-development.15aa0581.svg"
+  }
+]);
